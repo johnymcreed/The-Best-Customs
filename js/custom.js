6f72f2de240a784196a26dc2e9c21792388f7f4e
Your Fun Guy <78428114+The-Imortal-Crasher@users.noreply.github.com>
2021-12-06 14:37:13 -0600

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,29 +1,29 @@
-         document.addEventListener('contextmenu', event => event.preventDefault());
-         document.onkeydown = function(e) {
-            if(event.keyCode == 123) {
-               return false;
-            }
-            if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)){
-               return false;
-            }
-            if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
-               return false;
-            }
-            if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
-               return false;
-            }
-            if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
-               return false;
-            }
-         }
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.onkeydown = function(e) {
+if(event.keyCode == 123) {
+   return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)){
+   return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+   return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+   return false;
+}
+if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+   return false;
+}
+}
 
 
 (function( d ) {
    'use strict';
    var element = d.getElementById( 'cookie-banner' );
-       element.classList.add( localStorage.getItem( 'cookie-btn' ) );
+       element.classList.add( localStorage.getItem( 'remove-cookie' ) );
        element.addEventListener( 'click',
          function(){
-                     localStorage.setItem( 'cookie-banner', 'hide' );
+                     localStorage.setItem( 'remove-cookie', 'hide' );
                    }, false);
 }( document )); 
